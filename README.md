153f707901a012281bf7ec4975bdefc0b77ee218
Dru Goradia <dru@atlasdigital.tv>
2015-04-21 13:30:13 -0700

    ChefDK Wercker box

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# chefdk-builder
+# ChefDK Wercker Box
 Wercker Box for building Chef cookbooks
+
+* ChefDK 0.4.0
---
+++ script.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Some colors
+yellow='\033[1;33m'
+NC='\033[0m'
+
+# Required dependencies
+sudo apt-get -y install git
+sudo apt-get -y update
+
+# Check if exists and install
+dpkg-query -l "chefdk" | grep -q ^.i
+if [ $? -ne 0 ]; then
+	echo -e "${yellow}Downloading ChefDK${NC}"
+	export CHEFDK_INSTALLER=chefdk_0.4.0-1_amd64.deb
+	sudo wget -nv -N -P /var/cache/downloads https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/$CHEFDK_INSTALLER
+	echo -e "${yellow}Installing ChefDK${NC}"
+	sudo dpkg -i /var/cache/downloads/$CHEFDK_INSTALLER
+	chef -v
+
+	# Configure system Ruby
+	echo -e "${yellow}Configuring System Ruby${NC}"
+	echo 'eval "$(chef shell-init bash)"' | sudo tee --append /etc/profile > /dev/null
+	source /etc/profile
+	which ruby
+fi
+
+
+
+# Remove installer
+rm -rf /var/cache/downloads/$CHEFDK_INSTALLER
