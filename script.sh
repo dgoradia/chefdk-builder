472ca2a2f2d1b9bae312c1d7807b6198651483fb
Dru Goradia <dru@atlasdigital.tv>
2015-05-01 17:03:58 -0700

    Latest working build

--- script.sh
+++ script.sh
@@ -5,14 +5,14 @@ yellow='\033[1;33m'
 NC='\033[0m'
 
 # Required dependencies
-sudo apt-get -y install git build-essential
+sudo apt-get -y install git build-essential autoconf
 sudo apt-get -y update
 
 # Check if exists and install
 dpkg-query -l "chefdk" | grep -q ^.i
 if [ $? -ne 0 ]; then
 	echo -e "${yellow}Downloading ChefDK${NC}"
-	export CHEFDK_INSTALLER=chefdk_0.4.0-1_amd64.deb
+	export CHEFDK_INSTALLER=chefdk_0.5.1-1_amd64.deb
 	sudo wget -nv -N -P /var/cache/downloads https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/$CHEFDK_INSTALLER
 	echo -e "${yellow}Installing ChefDK${NC}"
 	sudo dpkg -i /var/cache/downloads/$CHEFDK_INSTALLER
@@ -24,6 +24,9 @@ if [ $? -ne 0 ]; then
 	source /etc/profile
 	which ruby
 fi
+
+# Not needed, possible future use.
+printf "source 'https://rubygems.org'%s\n%s\nruby '2.1.5'%s\n%s\ngem 'rake', '10.4.2'%s\ngem 'chef', '12.0.3'%s\ngem 'berkshelf', '3.2.3'%s\ngem 'chef-vault', '2.5.0'%s\ngem 'chefspec', '4.2.0'%s\ngem 'foodcritic', '4.0.0'%s\ngem 'test-kitchen'" > Gemfile
 
 # Remove installer
 rm -f /var/cache/downloads/$CHEFDK_INSTALLER
