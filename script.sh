c7c8744c14cd00b9b95664a69bc14cf64126d79f
Dru Goradia <dru@atlasdigital.tv>
2016-02-20 22:47:50 -0800

    Updates tp ChefDK 0.10.0

--- script.sh
+++ script.sh
@@ -12,7 +12,7 @@ sudo apt-get -y update
 dpkg-query -l "chefdk" | grep -q ^.i
 if [ $? -ne 0 ]; then
 	echo -e "${yellow}Downloading ChefDK${NC}"
-	export CHEFDK_INSTALLER=chefdk_0.5.1-1_amd64.deb
+	export CHEFDK_INSTALLER=chefdk_0.10.0-1_amd64.deb
 	sudo wget -nv -N -P /var/cache/downloads https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/$CHEFDK_INSTALLER
 	echo -e "${yellow}Installing ChefDK${NC}"
 	sudo dpkg -i /var/cache/downloads/$CHEFDK_INSTALLER
@@ -36,9 +36,6 @@ if [ $? -ne 0 ]; then
 	sudo dpkg -i /var/cache/downloads/vagrant_1.7.2_x86_64.deb
 	vagrant -v
 fi
-
-# Not needed, possible future use.
-printf "source 'https://rubygems.org'%s\n%s\nruby '2.1.5'%s\n%s\ngem 'rake', '10.4.2'%s\ngem 'chef', '12.0.3'%s\ngem 'berkshelf', '3.2.3'%s\ngem 'chef-vault', '2.5.0'%s\ngem 'chefspec', '4.2.0'%s\ngem 'foodcritic', '4.0.0'%s\ngem 'test-kitchen'" > Gemfile
 
 # Remove installer
 rm -f /var/cache/downloads/$CHEFDK_INSTALLER
